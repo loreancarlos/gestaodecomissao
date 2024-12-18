291feabbc7a9166a0266a07b4cf83cc59b22e74b
Lorean Carlos Fernandes Soares <lorean.carlos@gmail.com>
2024-12-18 13:06:30 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 //export const API_URL = "http://localhost:3000/api";
-export const API_URL = "https://apigestaodecomissao.onrender.com/api";
+//export const API_URL = "https://apigestaodecomissao.onrender.com/api";
+export const API_URL = "http://191.252.220.154:3000/api"
